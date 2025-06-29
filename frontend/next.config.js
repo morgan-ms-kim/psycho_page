7ea72ad3d7a673c53988f9f9ffe131989ccdbcf8
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-06-29 18:08:03 +0900

    경로수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/psycho_page',
   trailingSlash: false,
   compiler: {
     styledComponents: true,
   },
   experimental: {
     esmExternals: 'loose',
+  },
+  async redirects() {
+    return [
+      {
+        source: '/tests/:path*',
+        destination: '/psycho_page/tests/:path*',
+        permanent: false,
+      },
+    ];
   },
 };
 
