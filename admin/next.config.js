ff427888b5683ed304225582065fb6f50ed50fcd
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-06-30 06:57:57 +0900

    경
    로

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   basePath: '/psycho_page/admin',
-  assetPrefix: '/psycho_page/admin',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/psycho_page/admin' : '',
+  trailingSlash: false,
+  // 정적 파일 경로 설정
+  distDir: '.next',
+  // 정적 파일 경로를 basePath에 맞게 설정
   compiler: {
     styledComponents: true,
   },
