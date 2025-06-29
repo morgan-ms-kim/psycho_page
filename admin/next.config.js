a88e7ff2f864eda0c94c9448443d54138eae29e4
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-06-29 22:47:39 +0900

    관리자 페이지의 next.config.js에서 basePath를 수정
    관리자 페이지의 next.config.js에서 basePath를 수정

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/psycho_page/admin',
   trailingSlash: false,
   compiler: {
     styledComponents: true,
@@ -7,6 +8,8 @@ const nextConfig = {
   experimental: {
     esmExternals: 'loose',
   },
+  // assetPrefix도 basePath와 일치시킴
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/psycho_page/admin' : '',
   // 서버 설정 추가
   poweredByHeader: false,
   compress: true,
