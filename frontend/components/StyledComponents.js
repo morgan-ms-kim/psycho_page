62a41bd6a1147999757b5e468c7eb3c1e8c6b3e2
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-07-03 18:00:11 +0900

    최최최최종

--- frontend/components/StyledComponents.js
+++ frontend/components/StyledComponents.js
@@ -50,7 +50,7 @@ export const LoadingWrap = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  background: transparent;
   color: white;
 `;
 
