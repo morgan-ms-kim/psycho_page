d800360c5e527e86d21c0422ef8cd9ec6d9fbb7c
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-07-09 07:44:40 +0900

    next문제
    next문제

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,16 +7,6 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
-  webpack(config) {
-    // tests 폴더 내 css는 null-loader로 처리
-    config.module.rules.push({
-      test: /\.css$/,
-      include: path.resolve(__dirname, 'tests'),
-      use: 'null-loader',
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
