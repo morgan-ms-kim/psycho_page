3b5fff43d9f854d5135682f536731a3f93ce24f4
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-07-15 23:55:44 +0900

    ㄹㅁㄹ

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-
+console.log('DB_USER:', process.env.DB_USER);
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
