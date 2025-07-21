1a2197b7f314f40cda7798edcf1c9a7e596bcf81
morgan-ms-kim <morgan.ms.kim@gmail.com>
2025-07-21 17:14:58 +0900

    n

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //basePath: '/admin',
-  //assetPrefix: '/admin',
+  basePath: '/admin',
+  assetPrefix: '/admin',
   compiler: {
     styledComponents: true,
   },
